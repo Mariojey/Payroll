722b81c0fa21c41960f52dbefba32e2ba02a3044
MarioJey <marios.yaceg@gmail.com>
2023-03-08 10:05:39 +0100

    getting Employee By Id has been added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListView from "./components/cruds/listView";
 import Create from "./components/cruds/createForm";
+import EmployeeCard from "./components/cruds/employeeById";
 
 function App(){
 
@@ -11,6 +12,7 @@ function App(){
         <Routes>
           <Route exact path="/" element={<ListView />} />
           <Route exact path="/create" element={<Create />} />
+          <Route exact path="/employee/:id" element={<EmployeeCard />} />
         </Routes>
       </Router>
     </div>
